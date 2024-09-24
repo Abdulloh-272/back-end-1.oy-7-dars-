3849873956d4811f5e215bda37e3758e85894682
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-09-24 21:12:31 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+ism = input('Ismingizni kiriting: ')
+
+ism_palindrom = ism[::-1] == ism
+
+print(ism_palindrom)
+
+
+parol_1 = int(input('Birinchi parolni kiriting: '))
+parol_2 = int(input('Ikkinchi parolni kiriting: '))
+
+equal = parol_1 == parol_2
+print(equal)
+
+son = int(input('Son kiriting: '))
+
+son_juft = son % 2 ==0
+
+print(son_juft)
+
